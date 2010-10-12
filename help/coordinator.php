baaab2c66bab69315ba30979bf0810376821ca14
William Mensah <w.w.mensah@gmail.com>
2010-10-11 23:51:13 -0400

    still cleaning

--- help/coordinator.php
+++
@@ -1,6 +0,0 @@
-<?
-$data = $_GET['data'];
-
-//include the file
-include $data . '.html';
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<? ob_start(); ?>
-<?php require_once("includes/session.php"); ?>
-<?php require_once("includes/functions.php"); ?>
-<?php
-$_SESSION= array();
-    redirect_to("index.php");
-?>
-<? ob_flush(); ?>
