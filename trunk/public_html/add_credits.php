c4ef72a1ade2db48e6c91448efaf8cb688119683
jimethn <jimethn@localhost>
2010-11-01 18:34:16 -0400

    Buying credits now works * add_credits.php now has a form with a paypal purchase button * confirm_puchase.php processes payment data and adds credits to the user * for this to work you must turn on auto-return and PDT in paypal and set the url to the confirm page

--- trunk/public_html/add_credits.php
+++ trunk/public_html/add_credits.php
@@ -1,12 +1,29 @@
 <?php
 include 'init_utils.php';
 
-$page_title = "CBE Home | " . ucwords($_SESSION['fullname']);
+$page_title = "Add credits | " . ucwords($_SESSION['fullname']);
 include 'layout/startlayout.php';
-nav_menu($_SESSION['username'], 'home');
+nav_menu($_SESSION['username'], '');
 ?>
 
-Coming soon...
+<div class="page_info">
+
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="866FRXJTAL374">
+<table>
+<tr><td><input type="hidden" name="on0" value="Book credits">Book credits</td></tr><tr><td><select name="os0">
+	<option value="1">1 $1.30</option>
+	<option value="5">5 $5.00</option>
+	<option value="10">10 $9.00</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</div>
 
 <?php
 include 'layout/endlayout.php';
