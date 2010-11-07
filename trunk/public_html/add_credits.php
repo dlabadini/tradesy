e594120d2923cfad8b8ab26652937c03fd4c2931
devinlab77 <devinlab77@localhost>
2010-11-07 14:41:50 -0500

    added missing </div>

--- trunk/public_html/add_credits.php
+++ trunk/public_html/add_credits.php
@@ -28,7 +28,7 @@ nav_menu($_SESSION['username'], '');
 
 <div class='action' id='add_credits' style='display:; position:relative;'>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick"><font color="#0000A0"><h2>Add Credits<h2/><font/>
+<input type="hidden" name="cmd" value="_s-xclick"><font color="#0000A0"><h2>Add Credits<h2/></font>
 <input type="hidden" name="hosted_button_id" value="CRWXL494AZHFS">
 <table CELLPADDING=3>
 <tr><td><input type="hidden" name="on0" value="Credits"></td><td><select name="os0">
@@ -42,9 +42,10 @@ nav_menu($_SESSION['username'], '');
 <input type="hidden" name="currency_code" value="USD">
 <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form></td><tr></table>
+</td><tr></table>
+</form>
 </div>
-
+</div>
 
 <?php
 include 'layout/endlayout.php';
