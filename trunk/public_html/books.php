05e7f8f3cfe41018bc5ced7dda5f38891de20eb1
devinlab77 <devinlab77@localhost>
2010-11-07 00:26:26 -0400

    some aesthetic changes lol

--- trunk/public_html/books.php
+++ trunk/public_html/books.php
@@ -83,24 +83,19 @@ function limitText(limitField, limitCount, limitNum) {
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="CRWXL494AZHFS">
-<table>
-<tr><td><input type="hidden" name="on0" value="Credits"></td></tr><tr><td><select name="os0">
+<table CELLPADDING=3>
+<tr><td><input type="hidden" name="on0" value="Credits"></td><td><select name="os0">
 	<option value="1 Credit">1 Credit $1.50</option>
 	<option value="2 Credits">2 Credits $3.00</option>
 	<option value="3 Credits">3 Credits $4.50</option>
 	<option value="4 Credits">4 Credits $6.00</option>
 	<option value="5 Credits">5 Credits $6.25</option>
 	<option value="10 Credits">10 Credits $10.00</option>
-</select> </td></tr>
-</table><br/>
+</select> </td><td>
 <input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form><table><tr><td>
-</td><td>
-</td><td width='60px'>
-</td><td>
-</td></tr></table>
+</form></td><tr></table>
 </div>
 
 
