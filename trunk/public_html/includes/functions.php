82117e778624f88951feec4dc532b2088f754e5c
jimethn <jimethn@localhost>
2010-11-18 18:11:31 -0500

    added a link to do outer search of no results found

--- trunk/public_html/includes/functions.php
+++ trunk/public_html/includes/functions.php
@@ -1295,6 +1295,19 @@ function subscription_type($userid){
   $sql = "SELECT subscription_id FROM member_subscriptions WHERE member_id = $userid";
   return mysql_result(mysql_query($sql), 0, 0);
 }
+
+function curPageURL() {
+ $pageURL = 'http';
+ if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ return $pageURL;
+}
+
 ?>
 
 
